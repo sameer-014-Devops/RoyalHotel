8c2d7fc3fd7791a03e00a92b9798fd9774c38d98
Sameer A <sksameer4970@gmail.com>
2025-02-14 17:06:35 +0530

    Update RoyalHotel_Testing_Pipeline.groovy

--- Jenkins/RoyalHotel_Testing_Pipeline.groovy
+++ Jenkins/RoyalHotel_Testing_Pipeline.groovy
@@ -261,7 +261,7 @@ pipeline {
             steps{
                 sh """mkdir -p $Prod_Workspace"""
                 sh """cp -R Dockerfile *.yaml target $Prod_Workspace"""
-                echo '**********Copied the Dockerfile, royalhoteldeploy.yaml, and target files to the Medicure Production workspace**********'
+                echo '**********Copied the Dockerfile, royalhoteldeploy.yaml, and target files to the RoyalHotel Production workspace**********'
                 cleanWs()
                 deleteDir()
             }
